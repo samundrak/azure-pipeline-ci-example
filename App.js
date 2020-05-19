6fce5cfc758e0dd450788ef7abc823e59128e042
Samundra Khatri <samundra.khatri@joinsage.com>
2020-05-19 22:53:39 +0545

    First small tiny minimum commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [timer, setTimer] = useState(0);
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(timer + 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return <div>I am running {timer} on every 5 second</div>;
+};
+export default App;
